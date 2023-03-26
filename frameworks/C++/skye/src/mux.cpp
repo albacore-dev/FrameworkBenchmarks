f261a57bac88af10c47b38ac964f0c06f398cfdb
Luke Tokheim <luketokheim@gmail.com>
2023-03-25 17:34:09 -0700

    Stub in /queries. Moved date updater to the mux header

--- frameworks/C++/skye/src/mux.cpp
+++ frameworks/C++/skye/src/mux.cpp
@@ -1,7 +1,12 @@
 #include "mux.hpp"
 #include "model.hpp"
 
+#include <boost/asio/system_timer.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/url/parse.hpp>
 #include <fmt/chrono.h>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
 
 namespace skye_benchmark {
 
@@ -14,6 +19,7 @@ asio::awaitable<skye::response> Mux::operator()(skye::request req) const
 {
     constexpr std::string_view kJsonPath = "/json";
     constexpr std::string_view kDbPath = "/db";
+    constexpr std::string_view kQueriesPath = "/queries";
     constexpr std::string_view kPlaintextPath = "/plaintext";
 
     if (req.method() != http::verb::get) {
@@ -24,20 +30,13 @@ asio::awaitable<skye::response> Mux::operator()(skye::request req) const
         co_return json(req);
     } else if (req.target() == kDbPath) {
         co_return db(req);
+    } else if (req.target().starts_with(kQueriesPath)) {
+        co_return queries(req);
     } else if (req.target() == kPlaintextPath) {
         co_return plaintext(req);
     }
 
     co_return make_response(http::status::not_found, req);
-}
-
-skye::response Mux::plaintext(const skye::request& req) const
-{
-    auto res = make_response(http::status::ok, req);
-    res.set(http::field::content_type, kContentTypeText);
-    res.body() = kHelloMessage;
-
-    return res;
 }
 
 skye::response Mux::json(const skye::request& req) const
@@ -62,6 +61,29 @@ skye::response Mux::db(const skye::request& req) const
     return res;
 }
 
+skye::response Mux::queries(const skye::request& req) const
+{
+    // auto url = boost::urls::parse_origin_form(req.target());
+    // auto params = url.params();
+
+    const std::vector<World> models{{1, 10000}, {2, 9999}};
+
+    auto res = make_response(http::status::ok, req);
+    res.set(http::field::content_type, kContentTypeJson);
+    res.body() = fmt::format("{}", models);
+
+    return res;
+}
+
+skye::response Mux::plaintext(const skye::request& req) const
+{
+    auto res = make_response(http::status::ok, req);
+    res.set(http::field::content_type, kContentTypeText);
+    res.body() = kHelloMessage;
+
+    return res;
+}
+
 skye::response
 Mux::make_response(http::status status, const skye::request& req) const
 {
@@ -72,6 +94,21 @@ Mux::make_response(http::status status, const skye::request& req) const
     res.set(http::field::date, fmt::format("{:%a, %d %b %Y %T} GMT", ctx->now));
 
     return res;
+}
+
+asio::awaitable<void> async_poll_date(std::shared_ptr<Context> ctx)
+{
+    using namespace std::chrono_literals;
+
+    asio::system_timer timer{co_await asio::this_coro::executor};
+    for (;;) {
+        const auto now = std::chrono::system_clock::now();
+
+        ctx->now = fmt::gmtime(std::chrono::system_clock::to_time_t(now));
+
+        timer.expires_at(now + std::chrono::seconds{1});
+        co_await timer.async_wait(asio::use_awaitable);
+    }
 }
 
 } // namespace skye_benchmark
--- frameworks/C++/skye/src/mux.hpp
+++ frameworks/C++/skye/src/mux.hpp
@@ -17,14 +17,17 @@ struct Context {
 struct Mux {
     asio::awaitable<skye::response> operator()(skye::request req) const;
 
-    skye::response plaintext(const skye::request& req) const;
     skye::response json(const skye::request& req) const;
     skye::response db(const skye::request& req) const;
+    skye::response queries(const skye::request& req) const;
+    skye::response plaintext(const skye::request& req) const;
 
     skye::response
     make_response(http::status status, const skye::request& req) const;
 
     std::shared_ptr<Context> ctx;
 };
+
+asio::awaitable<void> async_poll_date(std::shared_ptr<Context> ctx);
 
 } // namespace skye_benchmark
